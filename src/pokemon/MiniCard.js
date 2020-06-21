db4b387d31507d0e17d6cd546e05268348a52536
Stolyar100 <olejer555@gmail.com>
2020-06-21 14:59:39 +0300

    rewrited MiniCard to functional component

--- src/pokemon/MiniCard.js
+++ src/pokemon/MiniCard.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import './MiniCard.css';
 
-export default class MiniCard extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            name: 'wartotle',
-            image:
-                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png',
-        };
-    }
-
-    render() {
-        return (
-            <div className="mini-card">
-                <div className="mini-card__image-block">
-                    <img
-                        className="mini-card__image"
-                        src={this.state.image}
-                        alt={this.state.name}
-                    />
-                </div>
-                <h2 className="mini-card__name">{this.state.name}</h2>
+export default function MiniCard({name = 'wartotle', image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png'}) {
+    return (
+        <div className="mini-card">
+            <div className="mini-card__image-block">
+                <img
+                    className="mini-card__image"
+                    src={image}
+                    alt={name}
+                />
             </div>
-        );
-    }
+            <h2 className="mini-card__name">{name}</h2>
+        </div>
+    );
 }
