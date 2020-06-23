33556b651cedcf0c82a19c0c689619fe20ec2ad2
Stolyar100 <olejer555@gmail.com>
2020-06-23 08:17:42 +0300

    added new action to get pokemon info

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -1,5 +1,6 @@
 import store from '../store';
-import {GET_POKEMON_LIST} from './types';
+import {GET_POKEMON_LIST, GET_POKEMON_INFO, GET_POKEMON_DESCRIPTION} from './types';
+import { pokemonListURL } from '../API/requestUrl';
 const axios = require('axios').default;
 
 const getPokemonList = (list) => ({
@@ -13,3 +14,15 @@ export const AsyncGetPokemonList = (url) =>
     axios.get(url)
     .then(res => res.data)
     .then(list => store.dispatch(getPokemonList(list)));
+
+const getPokemonInfo = (id, pokemonInfo) => ({
+    type: GET_POKEMON_INFO,
+    id: id,
+    image: pokemonInfo.sprites.front_default,
+    stats: pokemonInfo.stats,
+})
+
+export const AsyncGetPokemonInfo = (id, url) => 
+    axios.get(url)
+    .then(res => res.data)
+    .then(pokemonInfo => store.dispatch(getPokemonInfo(id, pokemonInfo)))
