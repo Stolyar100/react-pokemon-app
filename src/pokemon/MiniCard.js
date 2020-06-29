b1c049d87f91e87ed90e1a1bbd5f910cc7c55f8c
Stolyar100 <olejer555@gmail.com>
2020-06-29 13:24:41 +0300

    trasnformed MiniCard to class component, set AsyncGetPokemonInfo on mount
    trasnformed MiniCard to class component, for using lifecycle methods

--- src/pokemon/MiniCard.js
+++ src/pokemon/MiniCard.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import './MiniCard.css';
-import '../actions/requests'
+import {AsyncGetPokemonInfo} from '../actions/requests';
 
-export default function MiniCard({name = 'loading', image = '', url, id}) {
-    if (image === '') {
-        
+export default class MiniCard extends Component {
+    constructor(props) {
+        super(props)
     }
-    return (
-        <div className="mini-card">
-            <div className="mini-card__image-block">
-                <img
-                    className="mini-card__image"
-                    src={image}
-                    alt={name}
-                />
+
+    componentDidMount() {
+        AsyncGetPokemonInfo(this.props.id, this.props.url);
+    }
+    
+    render() {
+        const {name, image} = this.props;
+        return (
+            <div className="mini-card">
+                <div className="mini-card__image-block">
+                    <img
+                        className="mini-card__image"
+                        src={image}
+                        alt={name}
+                    />
+                </div>
+                <h2 className="mini-card__name">{name}</h2>
             </div>
-            <h2 className="mini-card__name">{name}</h2>
-        </div>
-    );
+        );
+    }
 }
+
