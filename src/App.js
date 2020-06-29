4ed35aab6d37a4012c39af6e691038e717446dc6
Stolyar100 <olejer555@gmail.com>
2020-06-29 17:42:40 +0300

    added router wraper to Layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Layout from './Layout';
+import Search from './navigation/Search'
+import List from './pokemon/List'
+import Card from './pokemon/Card';
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <BrowserRouter>
+        <Layout>
+          <Search />
+          <Switch>
+            <Route exact path="/">
+              <List />
+            </Route>
+            <Route path="/pokemon">
+              <Card />
+            </Route>
+          </Switch>
+        </Layout>
+      </BrowserRouter>
     </div>
   );
 }
