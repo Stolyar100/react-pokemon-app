5863c65212cf1ff0d7a4a7433f437779bc6ed1c2
Stolyar100 <olejer555@gmail.com>
2020-06-29 16:23:53 +0300

    resolved conflict in List.js

--- src/pokemon/List.js
+++ src/pokemon/List.js
@@ -1,45 +1,42 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import {pokemonListURL} from '../API/requestUrl';
+import {AsyncGetPokemonList} from '../actions/requests'
 import MiniCard from './MiniCard';
 import './List.css';
 
+class List extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            
+        }
+    }
+    
+    componentDidMount() {
+        AsyncGetPokemonList(pokemonListURL);
+    }
 
-export default class List extends Component {
     render() {
         return (
             <div className="list">
                 <div className="list__list">
-                    <div className="list__list-row">
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                    </div>
-                    <div className="list__list-row">
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                    </div>
-                    <div className="list__list-row">
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                    </div>
-                    <div className="list__list-row">
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                    </div>
-                    <div className="list__list-row">
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                        <MiniCard />
-                    </div>
+                {this.props.list.map((pokemon, id) => (
+                        <MiniCard name={pokemon.name} url={pokemon.url} id={id}/>
+                    ))}
                 </div>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        list: state.listReducer.list,
+        nextPage: state.listReducer.nextPage,
+        previousPage: state.listReducer.previousPage
+    }
+}
+
+export default connect(mapStateToProps)(List)
