56a8b08ef8e5ed1af28f8e4b9a5438ad5c9c169c
Stolyar100 <olejer555@gmail.com>
2020-07-06 17:03:56 +0300

    added pokemon-species url

--- src/API/requestUrl.js
+++ src/API/requestUrl.js
@@ -1,2 +1,3 @@
 export const baseURL = 'https://pokeapi.co/api/v2/';
 export const pokemonListURL = 'https://pokeapi.co/api/v2/pokemon/';
+export const pokemonSpeciesURL = ' https://pokeapi.co/api/v2/pokemon-species/';
