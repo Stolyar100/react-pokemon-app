3eb6baa8ba5d347bcfd31cae68c46270b977f795
Stolyar100 <olejer555@gmail.com>
2020-07-09 08:57:20 +0300

    updated list reducer file with eslint fixes

--- src/reducers/list.js
+++ src/reducers/list.js
@@ -1,59 +1,62 @@
-import {GET_POKEMON_LIST, GET_POKEMON_INFO, GET_POKEMON_DESCRIPTION} from '../actions/types';
+import {
+  GET_POKEMON_LIST,
+  GET_POKEMON_INFO,
+  GET_POKEMON_DESCRIPTION,
+} from '../actions/types';
 
 const initialState = {
-    nextPage: '',
-    previousPage: '',
-    list: [],
-}
+  nextPage: '',
+  previousPage: '',
+  list: [],
+};
 
-export default function listReducer(state = initialState, {type, nextPage, previousPage, list, id, image, stats, flavorText}) {
-    switch (type) {
-        case GET_POKEMON_LIST:
-            return Object.assign({}, state,
-                {
-                    nextPage,
-                    previousPage, 
-                    list,
-                }
-            );
-        case GET_POKEMON_INFO:
-            return Object.assign({}, state, {
-                    list: state.list.map((pokemon, index) => {
-                        if (index == id) {
-                            return {
-                                ...pokemon,
-                                image, 
-                                stats
-                            }
-                        } else {
-                            return pokemon;
-                        }
-                    }) 
-            });
-        case GET_POKEMON_DESCRIPTION:
-            debugger;
-            let description = '';
-            flavorText.some(flavor => {
+export default function listReducer(
+  state = initialState,
+  { type, nextPage, previousPage, list, id, image, stats, flavorText }
+) {
+  switch (type) {
+    case GET_POKEMON_LIST:
+      return { ...state, nextPage, previousPage, list };
+    case GET_POKEMON_INFO:
+      return {
+        ...state,
+        list: state.list.map((pokemon, index) => {
+          if (index === id) {
+            return {
+              ...pokemon,
+              image,
+              stats,
+            };
+          }
+          return pokemon;
+        }),
+      };
+    case GET_POKEMON_DESCRIPTION:
+      return {
+        ...state,
+        list: state.list.map((pokemon, index) => {
+          if (index == id) {
+            const description = ((flavors = flavorText) => {
+              let description = '';
+              flavors.some((flavor) => {
                 if (flavor.language.name === 'en') {
-                    description = flavor.flavor_text;
-                    return true;
+                  description = flavor.flavor_text;
+                  return true;
                 }
                 return false;
-            });
-            return Object.assign({}, state, {
-                list: state.list.map((pokemon, index) => {
-                    if (index == id) {
-                        return {
-                            ...pokemon,
-                            description
-                        }
-                    } else {
-                        return pokemon;
-                    }
-                })
-            });
+              });
+              return description;
+            })();
+            return {
+              ...pokemon,
+              description,
+            };
+          }
+          return pokemon;
+        }),
+      };
 
-        default: 
-            return state;
-    }
+    default:
+      return state;
+  }
 }
