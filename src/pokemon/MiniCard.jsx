22e41722730bf307db44d8f89a0e44791f6e34fb
Stolyar100 <olejer555@gmail.com>
2020-07-11 11:27:40 +0300

    added props validation, formated code in MiniCard

--- src/pokemon/MiniCard.jsx
+++ src/pokemon/MiniCard.jsx
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { AsyncGetPokemonInfo } from '../actions/requests';
 import './MiniCard.css';
-import {AsyncGetPokemonInfo} from '../actions/requests';
 
 export default class MiniCard extends Component {
-    componentDidMount() {
-        AsyncGetPokemonInfo(this.props.id, this.props.url);
-    }
-    
-    render() {
-        const {name, image} = this.props;
-        return (
-            <div className="mini-card">
-                <div className="mini-card__image-block">
-                    <img
-                        className="mini-card__image"
-                        src={image}
-                        alt={name}
-                    />
-                </div>
-                <h2 className="mini-card__name">{name}</h2>
-            </div>
-        );
-    }
+  componentDidMount() {
+    AsyncGetPokemonInfo(this.props.id, this.props.url);
+  }
+
+  render() {
+    const { name, image } = this.props;
+    return (
+      <div className="mini-card">
+        <div className="mini-card__image-block">
+          <img className="mini-card__image" src={image} alt={name} />
+        </div>
+        <h2 className="mini-card__name">{name}</h2>
+      </div>
+    );
+  }
 }
 
+MiniCard.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  url: PropTypes.string,
+  id: PropTypes.number,
+};
+
+MiniCard.defaultProps = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  url: PropTypes.string,
+  id: PropTypes.number,
+};
