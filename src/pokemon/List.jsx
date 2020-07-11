27f5cedc33ad7d3dd688c7fd08894346807835c1
Stolyar100 <olejer555@gmail.com>
2020-07-11 11:26:55 +0300

    added props validation, formated code in List

--- src/pokemon/List.jsx
+++ src/pokemon/List.jsx
@@ -1,45 +1,65 @@
-import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import {pokemonListURL} from '../API/requestUrl';
-import {AsyncGetPokemonList} from '../actions/requests'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { pokemonListURL } from '../API/requestUrl';
+import { AsyncGetPokemonList } from '../actions/requests';
 import MiniCard from './MiniCard';
 import './List.css';
-import { Link } from 'react-router-dom';
 
 class List extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            
-        }
-    }
-    
-    componentDidMount() {
-        AsyncGetPokemonList(pokemonListURL);
-    }
+  componentDidMount() {
+    AsyncGetPokemonList(pokemonListURL);
+  }
 
-    render() {
-        return (
-            <div className="list">
-                {
-                    this.props.list.map((pokemon, id) => (
-                        <Link to={`/pokemon/${id}`} key={pokemon.name}>
-                            <MiniCard name={pokemon.name} url={pokemon.url} image={pokemon.image} id={id}/>
-                        </Link>
-                    ))
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="list">
+        {this.props.list.map((pokemon, id) => (
+          <Link to={`/pokemon/${id}`} key={pokemon.name}>
+            <MiniCard
+              name={pokemon.name}
+              url={pokemon.url}
+              image={pokemon.image}
+              id={id}
+            />
+          </Link>
+        ))}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        list: state.listReducer.list,
-        nextPage: state.listReducer.nextPage,
-        previousPage: state.listReducer.previousPage
-    }
-}
+List.propTypes = {
+  nextPage: PropTypes.string,
+  previousPage: PropTypes.string,
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      image: PropTypes.string,
+      url: PropTypes.string,
+    })
+  ),
+};
 
-export default connect(mapStateToProps)(List)
+List.defaultProps = {
+  nextPage: PropTypes.string,
+  previousPage: PropTypes.string,
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      image: PropTypes.string,
+      url: PropTypes.string,
+    })
+  ),
+};
+
+const mapStateToProps = (state) => {
+  return {
+    list: state.listReducer.list,
+    nextPage: state.listReducer.nextPage,
+    previousPage: state.listReducer.previousPage,
+  };
+};
+
+export default connect(mapStateToProps)(List);
